9825c55a372d03c7c986a5ff39039994bfc30d89
phil pastorek <ppastorek@virid.com>
2012-04-16 15:32:59 -0400

    added splash screen

--- src/com/virid/ViridDirectory/ViridDirectoryActivity.java
+++ src/com/virid/ViridDirectory/ViridDirectoryActivity.java
@@ -1,12 +1,14 @@
 package com.virid.ViridDirectory;
 
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
 import android.view.View;
-import android.view.Window;
 import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -19,14 +21,39 @@ public class ViridDirectoryActivity extends ListActivity {
 	protected SQLiteDatabase db;
 	protected Cursor cursor;
 	protected ListAdapter adapter;
+	protected Dialog mSplashDialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.main);		
 		super.onCreate(savedInstanceState);
+		
+		
+		MyStateSaver data = (MyStateSaver) getLastNonConfigurationInstance();
+	    if (data != null) {
+	        // Show splash screen if still loading
+	        if (data.showSplashScreen) {
+	            showSplashScreen();
+	        }
+	        setContentView(R.layout.main);   
+	        searchText = (EditText) findViewById(R.id.searchText);
+			db = (new DatabaseHelper(this)).getWritableDatabase();
+	 
+	        // Rebuild your UI with your saved state here
+	    } else {
+	        showSplashScreen();
+	        setContentView(R.layout.main);
+	        searchText = (EditText) findViewById(R.id.searchText);
+			db = (new DatabaseHelper(this)).getWritableDatabase();
+	        // Do your heavy loading here on a background thread
+	    }
+		
+		
+		
+		//setContentView(R.layout.main);		
+		
 	
-		searchText = (EditText) findViewById(R.id.searchText);
-		db = (new DatabaseHelper(this)).getWritableDatabase();
+//		searchText = (EditText) findViewById(R.id.searchText);
+//		db = (new DatabaseHelper(this)).getWritableDatabase();
 
 
 	}
@@ -52,5 +79,63 @@ public class ViridDirectoryActivity extends ListActivity {
 				new int[] { R.id.firstName, R.id.lastName, R.id.title, R.id.department });
 		setListAdapter(adapter);
 	}
+	
+	
+	
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+	    MyStateSaver data = new MyStateSaver();
+	    // Save your important data here
+	 
+	    if (mSplashDialog != null) {
+	        data.showSplashScreen = true;
+	        removeSplashScreen();
+	    }
+	    return data;
+	}
+	 
+	/**
+	 * Removes the Dialog that displays the splash screen
+	 */
+	protected void removeSplashScreen() {
+	    if (mSplashDialog != null) {
+	        mSplashDialog.dismiss();
+	        mSplashDialog = null;
+	    }
+	}
+	 
+	/**
+	 * Shows the splash screen over the full Activity
+	 */
+	protected void showSplashScreen() {
+	    mSplashDialog = new Dialog(this, R.style.SplashScreen);
+	    mSplashDialog.setContentView(R.layout.splash);
+	    mSplashDialog.setCancelable(false);
+	    mSplashDialog.show();
+	    
+	    TextView splashText = (TextView) mSplashDialog.findViewById(R.id.splashText);
+		splashText.setText(Html.fromHtml(getString(R.string.splashText)));
+	 
+	    // Set Runnable to remove splash screen just in case
+	    final Handler handler = new Handler();
+	    handler.postDelayed(new Runnable() {
+	      public void run() {
+	        removeSplashScreen();
+	      }
+	    }, 3000);
+	}
+	 
+	/**
+	 * Simple class for storing important data across config changes
+	 */
+	private class MyStateSaver {
+	    public boolean showSplashScreen = false;
+	    // Your other important fields here
+	}
+
+	
+	
+	
 	
 }
